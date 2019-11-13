dfed4255e893b51fc3de2cea9706ebb698f77a7c
admin <1647093413@qq.com>
2019-11-13 21:02:56 +0800

    提交到get仓库中

---
+++ src/main/java/com/itheima/get/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.get;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+        System.out.println("HelloWord");
+    }
+
+}
